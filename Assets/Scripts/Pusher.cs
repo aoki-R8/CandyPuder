f5bf144b49f71680c2286fc815e76b400a146944
aoki-R8 <sanjujian468@gmail.com>
2026-05-14 11:48:09 +0900

    [Add] Unity Data, Physics to the candy
    飴のオブジェクトに物理挙動を追加

---
+++ Assets/Scripts/Pusher.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Pusher : MonoBehaviour
+{
+    //アクセス修飾子
+    public float speed = 1f;
+    public float movePower = 5f;
+    private Vector3 startPosition;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startPosition = this.transform.position;
+        Debug.Log("ゲームが開始した");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // z軸の往復移動を行う
+        float z = Mathf.Sin(Time.time * speed) * movePower;
+
+        // 自身のローカル座標の位置を　最初の位置情報に　ｚ(sin波の変動値)を加算して返す
+        //this.transform,localPosition
+        //↑　このコードがアタッチ(入っている)されているオブジェクトのローカル座標の情報
+        this.transform.localPosition = startPosition + new Vector3(0, 0, z);
+    }
+}
